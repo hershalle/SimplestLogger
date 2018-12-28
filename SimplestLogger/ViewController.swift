a097f54b69981f48749d06e7a15e59eeb97ef870
Shai Balassiano <hershalle@gmail.com>
2018-12-28 21:19:34 +0200

    Initial Commit

---
+++ SimplestLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplestLogger
+//
+//  Created by Shai Balassiano on 28/12/2018.
+//  Copyright © 2018 Shai Balassiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
